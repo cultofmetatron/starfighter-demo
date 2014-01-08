b1d214a53b524b3c6612e23d21a9cd09bbace020
Peter de Croos <cultofmetatron@aumlogic.com>
2014-01-07 16:52:03 -0800

    got stuff on the screen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!');
+/* canvas experiment */
+/*jslint indent:2 */
+'use strict';
+(function(window, $) {
+  var loadCanvas = function() {
+    var context = $('canvas#drawable')[0].getContext("2d");
+    context.fillStyle = '#ffffaa';
+    context.fillRect(0, 0, 50, 50);
+
+    context.fillStyle = 'green';
+    context.font = '20px Sans-Serif';
+
+    context.textBaseLine = "top";
+    context.fillText("Hello World", 195, 80);
+
+    //now load an image
+    var starfighter = new Image();
+    starfighter.src = "/images/fighter.png";
+    starfighter.onload = function() {
+      context.drawImage(starfighter, 50, 100);
+    };
+
+
+  };
+
+
+  $(window).on('ready', loadCanvas);
+
+
+}).call(this, window, jQuery);
+
+
+

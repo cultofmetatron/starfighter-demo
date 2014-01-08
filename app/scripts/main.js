c73d68ae9689f9ca8c0e663d4612a4cb910455ca
Peter de Croos <cultofmetatron@aumlogic.com>
2014-01-08 13:08:00 -0800

    added requestAnimationFrame

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,25 +2,123 @@
 /*jslint indent:2 */
 'use strict';
 (function(window, $) {
-  var loadCanvas = function() {
-    var context = $('canvas#drawable')[0].getContext("2d");
-    context.fillStyle = '#ffffaa';
-    context.fillRect(0, 0, 50, 50);
 
-    context.fillStyle = 'green';
-    context.font = '20px Sans-Serif';
-
-    context.textBaseLine = "top";
-    context.fillText("Hello World", 195, 80);
-
-    //now load an image
-    var starfighter = new Image();
-    starfighter.src = "/images/fighter.png";
-    starfighter.onload = function() {
-      context.drawImage(starfighter, 50, 100);
+  var bind = function(fn, context) {
+    return function() {
+      fn.apply(context, arguments);
     };
+  };
+
+  var DrawableObject = function(src, options) {
+    var imgLoad = $.Deferred();
+    this.imgLoaded = imgLoad.promise();
+    options = options || {};
+    this.setPosition(options.xPos || 0, options.yPos || 0);
+    this.image = new Image();
+    this.image.src = src;
+
+    this.image.onload = bind(function() {
+      this.setScale(options.scale || 1);
+      imgLoad.resolve();
+    }, this);
+  };
+
+  DrawableObject.prototype = Object.create({});
+  DrawableObject.fn = DrawableObject.prototype;
+  DrawableObject.fn.setPosition = function(x, y) {
+    this.xPos = x;
+    this.yPos = y;
+    return this;
+  };
+
+  DrawableObject.fn.getPosition = function() {
+    return {
+      xPos: this.xPos,
+      yPos: this.yPos
+    };
+  };
+
+  DrawableObject.fn.setScale = function(scale) {
+    this.width  = Math.floor(this.image.width * scale);
+    this.height = Math.floor(this.image.height * scale);
+    return this;
+  };
+  DrawableObject.fn.draw = function(context) {
+    this.imgLoaded.done(bind(function() {
+      context.drawImage(this.image,this.xPos, this.yPos, this.width, this.height);
+    }, this));
+    return this;
+  };
 
 
+  var StarFighter = function(options) {
+    DrawableObject.call(this, '/images/fighter.png', options);
+    //this.bindKeyHandler('keydown',  this.moveDown);
+    this.bindKeyHandler('keyup'  ,  this.moveUp);
+    //this.bindKeyHandler('keyleft',  this.moveLeft);
+    //this.bindKeyHandler('keyright', this.moveRight);
+  };
+
+  StarFighter.prototype = Object.create(DrawableObject.prototype);
+  StarFighter.fn = StarFighter.prototype;
+  StarFighter.fn.bindKeyHandler = function(key, fn) {
+    $(window).on(key, _.bind(fn, this));
+  };
+
+  StarFighter.fn.moveLeft = function() {
+    var pos = this.getPosition();
+    this.setPosition(pos.xPos - 5, pos.yPos);
+  };
+
+  StarFighter.fn.moveRight = function() {
+    var pos = this.getPosition();
+    this.setPosition(pos.xPos + 5, pos.yPos);
+  };
+
+  StarFighter.fn.moveUp = function() {
+    var pos = this.getPosition();
+    this.setPosition(pos.xPos, pos.yPos - 5);
+  };
+
+  StarFighter.fn.moveDown = function() {
+    var pos = this.getPosition();
+    this.setPosition(pos.xPos, pos.yPos + 5);
+  };
+
+
+
+
+
+  
+  var drawCanvas = function(context, drawableObjects) {
+    _(drawableObjects).each(function(drawableObject) {
+      
+      drawableObject.draw(context);
+    });
+  };
+
+  var clearCanvas = function(context, width, height) {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+  };
+
+  var loadCanvas = function() {
+    var $canvas = $('canvas#drawable');
+    var canvasWidth  = $canvas.width();
+    var canvasHeight = $canvas.height();
+    var context = $canvas[0].getContext("2d");
+    var drawableObjects = [];
+
+    drawableObjects.push(new StarFighter({
+      xPos: 200,
+      yPos: 300,
+      scale: 0.5
+    }));
+
+    requestAnimationFrame(function() {
+      clearCanvas(context, canvasWidth, canvasHeight);
+      drawCanvas(context, drawableObjects);
+    });
   };
 
 
